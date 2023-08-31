2cee1152fb7d75c2c49f2551d3d42c72fa07596c
Eduardo Miguel Victoria Sánchez <eduardo.mvisa@gmail.com>
2023-08-31 10:19:26 -0600

    Database (#1)
    * Migrations created
    
    * Factories created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Course;
+use App\Models\Post;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)->create();
+        Category::factory(3)->create();
+        Course::factory(10)->create();
+        Post::factory(90)->create();
     }
 }
