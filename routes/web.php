7faf62c6a8a96bd38f3c4913dab06cd862291970
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-31 14:43:03 -0600

    PageController created

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
+use App\Http\Controllers\PageController;
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('course', [PageController::class, 'course'])->name('course');
 
 Route::middleware([
     'auth:sanctum',
