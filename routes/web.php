954fb1710219f8eccd2b80c47aa9a99b5980a4d3
eduardovisa <eduardo.mvisa@gmail.com>
2023-09-01 11:51:12 -0600

    Routes modified

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PageController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/courses', [PageController::class, 'home'])->name('courses');
 Route::get('course/{course:slug}', [PageController::class, 'course'])->name('course');
 
 Route::middleware([
