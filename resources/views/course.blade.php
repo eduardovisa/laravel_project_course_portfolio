1913287ec46e016e70fd6af1a9b576516632124e
eduardovisa <eduardo.mvisa@gmail.com>
2023-09-01 09:48:14 -0600

    Show similar courses

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -24,6 +24,18 @@
                     <p class="text-gray-300 text-xs">{{ $course->created_at->diffForHumans() }}</p>
                 </div>
             </div>
+            <div class="grid grid-cols-2 gap-4 my-8">
+                    @foreach ($course->similar() as $course)
+                        <div class="bg-white shadow-lg rounded-lg px-4 py-6">
+                            <a href="{{ route('course', $course->slug) }}">
+                                <img src="{{ $course->image }}" class="rounded-md mb-2">
+                                <h2 class="text-lg text-gray-600 truncate font-bold">{{ $course->name }}</h2>
+                                <h3 class="text-md text-gray-500">{{ $course->excerpt }}</h3>
+                                <img src="{{ $course->user->avatar }}" class="rounded-full mt-4 h-8 w-8">
+                            </a>
+                        </div>
+                    @endforeach
+            </div>
         </div>
     </div>
 
